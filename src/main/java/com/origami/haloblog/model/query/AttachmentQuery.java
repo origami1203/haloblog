5d0c3f3054eb8cbaa41348797fabf2940c47703d
origami <sppyml@163.com>
2020-12-16 20:07:22 +0800

    添加lombok注解

--- src/main/java/com/origami/haloblog/model/query/AttachmentQuery.java
+++ src/main/java/com/origami/haloblog/model/query/AttachmentQuery.java
@@ -1,12 +1,15 @@
 package com.origami.haloblog.model.query;
 
 import com.origami.haloblog.model.enums.AttachmentType;
+import lombok.Data;
 
 /**
  * @author origami1203
  * @date 2020-12-13 16:58
  * @description 附件的查询参数
  */
+
+@Data
 public class AttachmentQuery {
     /**
      * Keyword.
